a5061244e5c261b0f669081fd635d13f6394616f
LIO-H-ZEN <lio_h_zen@163.com>
2017-08-15 09:39:30 +0800

    finish simple unittest of utils_queue_h

--- unittest/utils/utils_queue.cpp
+++ unittest/utils/utils_queue.cpp
@@ -5,18 +5,21 @@
 namespace lzc {
 
 TEST(utils_queue_h, ts_queue_with_capacity) {
+    using namespace std::chrono_literals;
     ts_queue_with_capacity<int> que(1);
     int ind1 = 1;
     int ind2 = 1;
     std::thread t1([&]{
-        for(; ind1 < 10000; ++ind1)
+        for(; ind1 < 5; ++ind1)
             EXPECT_TRUE(que.push(ind1));
     });
     std::thread t2([&] {
-        for(; ind2 < 10000; ++ind2) {
+        for(; ind2 < 5; ++ind2) {
             int t;
+            std::this_thread::sleep_for(2s);
+            EXPECT_EQ(1, que.size());
             que.wait_pop(t);
-            EXPECT_TRUE(ind1 - 1 == t || t == ind1);
+            EXPECT_EQ(ind2, t);
         }
     });    
     t1.join();
