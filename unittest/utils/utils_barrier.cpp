52887d6e15596caf5bbd3eeba11ccc0492ffdf59
LIO-H-ZEN <lio_h_zen@163.com>
2017-08-17 21:58:36 +0800

    add barrier, udf_barrier and their unittests

--- unittest/utils/utils_barrier.cpp
+++ unittest/utils/utils_barrier.cpp
@@ -11,16 +11,42 @@ TEST(utils_barrier_h, cnt_barrier) {
     cnt_barrier cb(5);
     for (int i = 0; i < 4; ++i) {
         ts[i] = std::thread([&](int i){
-            std::cout << "thread id: " << i << "started" << std::endl;
+            std::cout << "thread id: " << i << " started" << std::endl;
             cb.block();
             que.push(i);
-            std::cout << "thread id: " << i << "ended" << std::endl;
+            std::cout << "thread id: " << i << " ended" << std::endl;
+        }, i);
+    }
+    ts[4] = std::thread([&]{
+        std::cout << "thread id: " << 4 << " started" << std::endl;
+        que.push(4);
+        cb.block();
+        std::cout << "thread id: " << 4 << " ended" << std::endl;
+    });
+    int first = -1;
+    que.wait_pop(first);
+    EXPECT_EQ(4, first);    
+    for (int i = 0; i < 5; ++i) {
+        ts[i].join();
+    }
+}
+
+TEST(utils_barrier_h, barrier) {
+    std::vector<std::thread> ts(5);
+    threadsafe_queue<int> que;
+    barrier b;
+    for (int i = 0; i < 4; ++i) {
+        ts[i] = std::thread([&](int i){
+            std::cout << "thread id: " << i << " started" << std::endl;
+            b.block();
+            que.push(i);
+            std::cout << "thread id: " << i << " ended" << std::endl;
         }, i);
     }
     ts[4] = std::thread([&]{
         std::cout << "thread id: " << 4 << "started" << std::endl;
         que.push(4);
-        cb.block();
+        b.unblock();
         std::cout << "thread id: " << 4 << "ended" << std::endl;
     });
     int first = -1;
@@ -30,4 +56,32 @@ TEST(utils_barrier_h, cnt_barrier) {
         ts[i].join();
     }
 }
+
+TEST(utils_barrier_h, udf_barrier) {
+    std::vector<std::thread> ts(5);
+    threadsafe_queue<int> que;
+    udf_barrier b;
+    bool ret = false;
+    for (int i = 0; i < 4; ++i) {
+        ts[i] = std::thread([&](int i){
+            std::cout << "thread id: " << i << " started" << std::endl;
+            b.block([&]{std::cout << i << " is block..." << std::endl;}, [&]{return ret;});
+            que.push(i);
+            std::cout << "thread id: " << i << " ended" << std::endl;
+        }, i);
+    }
+    ts[4] = std::thread([&]{
+        std::cout << "thread id: " << 4 << "started" << std::endl;
+        que.push(4);
+        b.unblock([&]{ret = true;});
+        std::cout << "thread id: " << 4 << "ended" << std::endl;
+    });
+    int first = -1;
+    que.wait_pop(first);
+    EXPECT_EQ(4, first);    
+    for (int i = 0; i < 5; ++i) {
+        ts[i].join();
+    }
+}
+
 }; // namespace lzc
--- utils/barrier.h
+++ utils/barrier.h
@@ -32,7 +32,54 @@ private:
     std::mutex _mu;
     std::condition_variable _block_cv;
 }; // class cnt_barrier
- 
-}; // namespace lzc
 
+// 基础的barrier 
+// block()阻塞调用方，unblock()释放全部阻塞线程
+class barrier : public barrier_base {
+public:
+    explicit barrier() {}
+
+    void block() {
+        std::unique_lock<std::mutex> ul(_mu);
+        _block_cv.wait(ul, [&]{return !still_block;});
+    }
+
+    void unblock() {
+        std::lock_guard<std::mutex> lg(_mu);
+        still_block = false;
+        _block_cv.notify_all();
+    }
+private:
+    std::mutex _mu;
+    std::condition_variable _block_cv;
+    bool still_block = true;
+}; // class barrier
+
+class udf_barrier : public barrier_base {
+    typedef std::function<void()> void_func;
+    typedef std::function<bool()> bool_func;
+public:
+    explicit udf_barrier() {}    
+
+    void block(const void_func &block_func, const bool_func &wait_func) {
+        std::unique_lock<std::mutex> ul(_mu);
+        block_func();
+        _block_cv.wait(ul, wait_func);
+    }
+
+    void unblock(const void_func &unblock_func) {
+        std::lock_guard<std::mutex> lg(_mu);
+        // remember change wait_func's return
+        unblock_func();
+        _block_cv.notify_all();        
+    }
+
+    virtual void block() {throw std::bad_function_call();};
+    virtual void unblock() {throw std::bad_function_call();};
+private:
+    std::mutex _mu;
+    std::condition_variable _block_cv;
+}; // class udf_barrier
+
+}; // namespace lzc
 #endif
