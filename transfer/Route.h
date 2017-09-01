f3e31b1ac2798d37256c7d06ce620e3c3fcbdad7
LIO-H-ZEN <lio_h_zen@163.com>
2017-09-01 14:49:59 +0800

    update transfer

--- transfer/Route.h
+++ transfer/Route.h
@@ -4,21 +4,34 @@
 #include "TFcommon.h"
 
 namespace lzc {
-
+// 维护着id和addr，mutex，socket的关系及相关的操作API
 class basic_route : public noncopyable_obj {
 public:
     explicit basic_route() {
         _zmq_ctx = zmq_ctx_new();
+        CHECK(NULL != _zmq_ctx);
     }
-    void add_node() {
+    // not thread safe
+    void add_node(int id, std::string addr) {
+        CHECK(true == _id_addrs.emplace(id, addr).second);  
+        CHECK(true == _id_mutexs.emplace(id, std::unique_ptr<std::mutex>(new std::mutex)).second)        
+        void *socket = zmq_socket(_zmq_ctx, ZMQ_PUSH);
+        CHECK(socket != NULL);
+        CHECK(true == _id_sockets.emplace(id, socket).second);     
+        // ignore_signal_call(zmq_connect, socket, addr.c_str());
+    }
+
+    void remove_node() {
 
     }
-    void remove_node() {
+    ~basic_route() {
 
     }
 private:
     void *_zmq_ctx = NULL;
-    
+    std::unordered_map<int, void *> _id_sockets;    
+    std::unordered_map<int, std::string> _id_addrs;    
+    std::unordered_map<int, std::unique_ptr<std::mutex>> _id_mutexs;    
 }; // class basic_route
 }; // namespace lzc
 #endif
--- transfer/TFcommon.h
+++ transfer/TFcommon.h
@@ -4,7 +4,22 @@
 #include <zmq.h>
 #include <unordered_map>
 
+#include "glog/logging.h"
+
 #include "../utils/NoncopyableObject.h"
 #include "../utils/Buffer.h"
+
+// 忽略异常每次都继续执行
+template <typename FUNC, typename... ARGS>
+void ignore_signal_call(FUNC func, ARGS &&... args) {
+    for (;;) {
+        auto ret = func(args);
+        if (ret < 0 && errno == EINTR) {
+            LOG(INFO) << "ignore_signal_call ignored a error";
+            continue;
+        }
+        break;
+    }
+}
 
 #endif
